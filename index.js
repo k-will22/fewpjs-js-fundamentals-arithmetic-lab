869553150c568080f761e6fd98728e09919fe127
k-will22 <thestreetkw@hotmail.com>
2020-12-14 16:12:46 -0500

    commit tree 75827c522a1d4b7cce826b8f54cfbae6f221372a

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 
 // Write your code here
+const num1 = 12.4
+
+const num2 = 5
+
+const multiply = num1 * num2
+
+const random = Math.floor(Math.random) + 1 
+
+const mod = 24 / 6
+
+const max = Math.max(4, 20, 17, 12, 3)
